29b6441ee52eac56ab13da7b5972c330e4a62fb0
Thiago <itsadeadh2@gmail.com>
2024-07-03 19:09:52 -0400

    Remove jsonify

--- src/domain/handlers/contact_handler.py
+++ src/domain/handlers/contact_handler.py
@@ -2,6 +2,7 @@ from src.infrastructure.exc import InvalidEmailError, PersistenceError, QueueInt
 from src.infrastructure.services import EmailDAO, Queue
 from flask import jsonify
 
+
 class ContactHandler:
     def __init__(
             self,
@@ -16,19 +17,20 @@ class ContactHandler:
             email = data.get('email', '')
             self.email.save(email)
             self.queue.add_to_queue(email)
-            return jsonify(message=f"Successfully received contact request. You should receive an email shortly on {email} with my contact information."), 200
+            return {
+                "message": f"Successfully received contact request. You should receive an email shortly on {email} with my contact information."}, 200
         except InvalidEmailError as e:
-            return jsonify(message=str(e)), 400
+            return {"message":str(e)}, 400
         except PersistenceError as e:
-            return jsonify(message=str(e)), 500
+            return {"message": str(e)}, 500
         except QueueInteractionError as e:
-            return jsonify(message=str(e)), 500
+            return {"message": str(e)}, 500
 
     def handle_get(self):
         try:
             emails = self.email.query()
-            return jsonify(emails=emails), 200
+            return {"emails": emails}, 200
         except DbLookupError as e:
             print(e)
-            return jsonify(mesage=str(e)), 500
+            return {"mesage": str(e)}, 500
 
