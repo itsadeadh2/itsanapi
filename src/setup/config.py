4595c7c66162bb9e3727973f6c9efe9020cc0a63
Thiago <itsadeadh2@gmail.com>
2024-07-07 02:52:37 -0400

    Remove dynamodb

--- src/setup/config.py
+++ src/setup/config.py
@@ -4,7 +4,6 @@ import os
 def add_configs(app):
     # Configs from env vars #
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'TOP SECRET KEY!')
-    app.config["TABLE_NAME"] = os.getenv("TABLE_NAME")
     app.config["QUEUE_URL"] = os.getenv("QUEUE_URL")
     app.config['GOOGLE'] = {
         'consumer_key': os.getenv('GOOGLE_CLIENT_ID'),
