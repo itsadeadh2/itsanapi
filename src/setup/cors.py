b14b1cfdc8bfa896f8c17cfe458888d0fa6adb7b
Thiago <itsadeadh2@gmail.com>
2024-07-09 22:03:42 -0400

    Leave cors open

--- src/setup/cors.py
+++ src/setup/cors.py
@@ -4,21 +4,4 @@ from flask_cors import CORS
 
 def add_cors(app):
     is_production = True if os.getenv("IS_PRODUCTION") else False
-    if is_production:
-        CORS(app, supports_credentials=True, resources={
-            r"/api/*": {
-                "origins": "https://commodore.itsadeadh2.com/",
-                "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-                "allow_headers": ["Content-Type", "X-CSRF-TOKEN"],
-                "expose_headers": ["X-CSRF-TOKEN"]
-            }
-        })
-    else:
-        CORS(app, supports_credentials=True, resources={
-            r"/api/*": {
-                "origins": ["http://localhost:3000", "http://127.0.0.1:3000"],
-                "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-                "allow_headers": ["Content-Type", "X-CSRF-TOKEN"],
-                "expose_headers": ["X-CSRF-TOKEN"]
-            }
-        })
+    CORS(app, supports_credentials=True)
